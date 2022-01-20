f8765eb750d6b7f4312466e462171409b307bf3e
conseicka <>
2022-01-20 13:49:20 -0600

    operations

--- act2.py
+++ act2.py
@@ -1,6 +1,10 @@
 from linkedList import SinglyLinkedList
 from array import Array
 
+
+words = SinglyLinkedList()
+
+
 theList = ["Nala", "Lisseth" , "hch", "Erick"]
 theArray = Array(len(theList))
 
@@ -8,10 +12,43 @@ for index in range(len(theList)):
     theArray.__setitem__(index, theList[index])
 
 
-words = SinglyLinkedList()
-
 for data in theArray:
     words.append(data)
 
+#for word in words.iter():
+ #   print(word)
+
+
+words.search("Lisseth")
+head = words.scan("a")
+print(head)
+
+print(words.replace_item("Erick", "David").data)
+
+print("-----"* 20)
+
+words.append("Green Day")
+
+words.insert_new_head("Zack")
+
+words.remove_node("hch")
+
+words.insert_node_before("Nala","Metarraluciano")
+
+words.insert_node_after("David", "Python")
+
+#words.delete_by_index(3)
+
+counter = 0
 for word in words.iter():
     print(word)
+    counter += 1
+
+print("-----"* 20)
+
+words.insert_node_by_index(2,"Leona")
+
+counter = 0
+for word in words.iter():
+    print(word)
+    counter += 1
--- linkedList.py
+++ linkedList.py
@@ -5,6 +5,13 @@ class SinglyLinkedList:
         self.tail = None
         self.size = 0
 
+    def __scan_before(self, data):
+        head = self.tail
+        while head != None and data != head.data:
+            probe = head
+            head = head.next
+        return probe, head
+
     def append(self, data):
         node = Node(data)
 
@@ -20,6 +27,8 @@ class SinglyLinkedList:
             current.next = node
 
         self.size += 1
+    
+
 
     def size(self):
         return str(self.size)
@@ -32,7 +41,82 @@ class SinglyLinkedList:
             current = current.next
             yield val
         
-    def delete(self, data):
+
+
+    def search(self, target_item):
+        head = self.tail
+        while head != None and target_item != head.data:
+            head = head.next
+       
+        if head is not None:
+            print(f"Data {target_item} found!")        
+        else:
+            print(f"Target item '{target_item}' is not in the linked list")
+
+        return head
+
+    def clear(self):
+        self.tail = None
+        self.head = None
+        self.size = 0
+
+
+    def scan(self, target_item):
+        head = self.tail
+        while head != None and target_item != head.data:
+            head = head.next
+ 
+        return head
+
+    def replace_item(self, target_item, new_value):
+        current = self.scan(target_item)
+        if current is not None:
+            current.data = new_value
+        
+        return current
+
+    def insert_new_head(self, new_value):
+        try:
+            self.tail = Node(new_value, self.tail)
+            self.size += 1
+        except:
+            return None
+    
+
+
+    def insert_node_before(self, index_node, insert_node):
+        probe, head = self.__scan_before(index_node)
+        try:
+            new_node = Node(insert_node,probe.next)
+            probe.next = new_node
+            self.size += 1
+        except:
+            return None
+    
+    def insert_node_after(self, index_node, insert_node):
+        head = self.scan(index_node)
+        new_node = Node(insert_node, head.next)
+        head.next = new_node
+        self.size += 1
+
+    def insert_node_by_index(self, index, data):
+        head = self.tail
+        probe = None
+        while index > 1 and head.next.next != None:
+            head = head.next
+            index -= 1
+        new_node = Node(data,head.next.next)
+        head.next = new_node
+
+    def remove_node(self, delet_node):
+        probe, head = self.__scan_before(delet_node)
+        try:
+            probe.next = head.next
+            self.size -= 1
+        except:
+            return None
+
+    def delet_by_data(self, data):
         current = self.tail
         previous = self.tail
 
@@ -46,17 +130,21 @@ class SinglyLinkedList:
                     return current.data
 
             previous = currentcurrent = current.next
+    
+    def delete_by_index(self, delete_inex):
+        head = self.tail
+        if delete_inex <= 0 or head.next is None:
+            removed_item = head.data
+            print(f"The item {removed_item} was removed")
+        else: 
+            probe = head
+            while delete_inex > 1 and probe.next.next != None:
+                probe = probe.next
+                delete_inex -= 1
+        removed_item = probe.next.data
+        probe.next = probe.next.next
+        print(f"the item {removed_item} was removed")
 
-    def search(self, data):
-        for node in self.iter():
-            if data == node:
-                print(f"Data {data} found!")
 
-    def clear(self):
-        self.tail = None
-        self.head = None
-        self.size = 0
-
-
-if __name__ == "__main__":
-    pass
+    if __name__ == "__main__":
+        pass
